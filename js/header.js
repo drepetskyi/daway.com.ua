dcbea602c392650aa93c29310d734a74f27f4f27
Dmytro <48658746+drepetskyi@users.noreply.github.com>
2021-08-19 17:21:41 +0300

    Add files via upload

---
+++ js/header.js
@@ -0,0 +1,73 @@
+// ----- const header
+const headerContainer = document.querySelector('.all-header');
+headerContainer.innerHTML += 
+`<nav>
+<div class="logo-img">
+<img class="img-header" src="../../img/logo.png" alt="картинка для хедера">
+<a href="../../index.html">daway.news</a>
+</div>
+<ul>
+<li> <a href = "../../events/events.html"> Події </a> </li>
+<li> <a href = "../../finance/finance.html"> Фінанси </a> </li>
+<li> <a href = "../../sports/sports.html"> Спорт </a> </li>
+<li> <a href = "../../life/life.html"> Життя </a> </li>
+</ul>
+<div class="mob-menu">
+<div class="btn-group">
+    <button type="button" class="btn btn-primiry dropdown-toggle" data-bs-toggle="dropdown">
+        menu
+    </button>
+    <ul class="dropdown-menu dropdown-menu-end">
+        <li> <button class="dropdown-item" type="button"><a href = "../../events/events.html">події</a></button></li>
+        <li> <button class="dropdown-item" type="button"><a href = "../../finance/finance.html">фінанси</a></button></li>
+        <li> <button class="dropdown-item" type="button"><a href = "../../sports/sports.html">спорт</a></button></li>
+        <li> <button class="dropdown-item" type="button"><a href = "../../life/life.html">життя</a></button></li>
+    </ul>
+</div>
+</div>
+</nav>
+<div class="yellow-line"></div>
+<div class="phrases"></div>
+<div class="date" id="date"></div>
+`
+// ----- current date and day
+const date = new Date();
+const days = ["неділя", "понеділок", "вівторок", "середа", "четвер", "п'ятниця", "субота"];
+const day = days[date.getDay()];
+const mounths = ["січня", "лютого", "березня", "квітня", "травня", "червня", "липня", "серпня", "вересня", "жовтня", "листопада", "грудня"];
+const mount = mounths[date.getMonth()];
+document.querySelector('.date').innerHTML = day + ' ' + date.getDate() + ' ' + mount + ' ' + date.getUTCFullYear();
+
+// ----- phrasese, random
+async function fetchJson() {
+    const response = await fetch('json/data-phrases.json');
+    const phrase = await response.json();
+    const phrasesContainer = document.querySelector('.phrases');
+    let phrasesDomSrring = '';
+    function getRandomInt(max) {
+        return Math.floor(Math.random() * Math.floor(max));
+    };
+    let i = getRandomInt(phrase.data.length);
+    phrasesDomSrring += `<div class="phrase"> ${phrase.data[i].phrase + ' ' + phrase.data[i].author}</div> `
+    phrasesContainer.innerHTML = phrasesDomSrring;
+};
+fetchJson();
+setInterval(fetchJson, 10000);
+
+
+// -------- footer for all
+document.querySelector('.main-footer').innerHTML += `
+<a href="../../index.html">daway.news</a>
+<a href="../../cookie.html">Про нас</a>
+<div class="text-for-footer"> created by D&A.way
+</div>
+<!-- <div class="login">
+    <a href="login.html">login</a>
+</div> -->
+
+<div id="consent-popup" class="hidden">
+<p>Продовжуючи перебувати на сайті Ви погоджуєтесь з <a href="https://daway.com.ua/cookie.html" id="condition"> умовами його використання</a>.
+    <a id="accept" href="#">Згоден</a>
+</p>
+</div>
+`
---
+++ js/news.js
@@ -0,0 +1,69 @@
+const dateCurr = new Date();
+const dateForCheckDate = (dateCurr.getMonth() + 1) + '-' + dateCurr.getDate() + '-' + dateCurr.getUTCFullYear();
+let addTextAboutAgo = "більше року тому";
+let acrticleContainer = document.querySelector('.art-news');
+let addNews = ``;
+
+async function feachNews() {
+
+    const newsdata = await (await fetch('../../json/data-all.json')).json();
+    const news = newsdata;
+
+    for (let i = news.all.length - 1; i >= 0; i--) {
+        let rezultInDays =  Math.round((Date.parse(dateForCheckDate) - Date.parse(news.all[i].publicDate)) / 1000 / 60 / 60 / 24);
+        switch (true) {
+            case rezultInDays === 0:
+                addTextAboutAgo = " сьогодні";
+                break;
+            case rezultInDays <= 1:
+                addTextAboutAgo = " вчора";
+                break;
+            case rezultInDays <= 4:
+                addTextAboutAgo = " " + rezultInDays + " дні тому";
+                break;
+            case rezultInDays <= 7:
+                addTextAboutAgo = " " + rezultInDays + " днів тому";
+                break;
+            case rezultInDays <= 30:
+                addTextAboutAgo = " понад тиждень тому";
+                break;
+            case rezultInDays <= 365:
+                addTextAboutAgo = " понад місяць тому";
+                break;
+        };
+        addNews += `<div class="card-news" cat="${news.all[i].cat}">
+                    <div class="text-for-news">
+                        <div class="part-one">
+                            <img class="img-for-news" src="../../${news.all[i].image}" alt="${news.all[i].altForImg}">
+                            <div class="headline"> <h1> <a>${news.all[i].headline} </a></h1>
+                                <div class="heading">${news.all[i].heading}</div>
+                            </div>
+                        </div>
+                        <div id="fb-root"></div>
+                        <p class="part-start"> <a href="../../${news.all[i].link}"> Читати більше...</a></p>
+                        <div class="published" pub="${news.all[i].publicDate}"> Опубліковано ${addTextAboutAgo}</div>
+                    </div>
+                </div>
+                <div class="fb-comments" data-href="https://daway.com.ua" data-width="100%" data-numposts="1"></div>`;
+
+
+        acrticleContainer.innerHTML = addNews;
+    }
+
+    const cardsNews = document.querySelectorAll('.card-news');
+    const filter = document.querySelector('.art-news').getAttribute('category');
+    cardsNews.forEach(elem => {
+        elem.classList.remove('hide');
+        if (elem.getAttribute('Cat') !== filter) {
+            elem.classList.add('hide');
+        }
+    });
+
+};
+
+feachNews();
+
+
+
+
+
